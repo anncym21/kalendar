7caa8b83ba2e7d3be46d3e27d7de8e2886c069f8
student <cymanowska.anna@wp.pl>
2022-04-22 14:45:02 +0200

    zmiany w activity main

--- app/src/main/java/com/example/kalendar/MainActivity.kt
+++ app/src/main/java/com/example/kalendar/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kalendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CalendarView
+import android.widget.ProgressBar
 import android.widget.SeekBar
 import android.widget.TextView
 
@@ -9,21 +11,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Long min = c.getTime().getTime()
-        Long max = 21772800L + c.gettime().getTime()
-        CalendarView calendar1 = (CalendarView) findViewById(R.id.calendar1)
-        calendar1.setMinDate(min)
-        calendar1.setMaxdate(max)
-        calendarView calendar2  (CalendarView) findViewById(R.id.calendar2)
 
-        progressBar = (Progressbar) findViewById(R.id.progressBar)
-        seekBar = (Seekbar) findViewById(R.id.seekBar)
-        textView = (TextView) findViewById(R.id.text1)
+        val CalendarView = findViewById<CalendarView>(R.id.calendar1)
+        val progressBar =  findViewById<ProgressBar>(R.id.progressBar)
+        val seekBar =  findViewById<SeekBar>(R.id.seekBar)
+        val textView = findViewById<TextView>(R.id.text1)
 
         seekBar.setOnSeekBarChangeListener(new Seekbar.OnSeekBarChangeListener()){
             public void onProgressChanged(SeekBar seekbar, int progress, boolean fromUser){
                 progressBar.setProgress(progress);
-                textView.setText(" "+ progress+ "%")
+                textView.setText(" "+ progress+ " %")
             }
             public void onStartTrackingTouch(SeekBar seekbar){
 
