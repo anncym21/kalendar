2dc8651b7864488dc320fdd66a6c01a779a125ec
student <cymanowska.anna@wp.pl>
2022-04-22 12:16:44 +0200

    data min i max

--- app/src/main/java/com/example/kalendar/MainActivity.kt
+++ app/src/main/java/com/example/kalendar/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Long min = c.getTime().getTime()
+        Long max = 21772800L + c.gettime().getTime()
+        CalendarView calendar1 = (CalendarView) findViewById(R.id.calendar1)
+        calendar1.setMinDate(min)
+        calendar1.setMaxdate(max)
+        calendarView calendar2  (CalendarView) findViewById(R.id.calendar2)
+        if(calendar1<calendar2){
+
+        }
     }
 }
